ac9d9db4f38d93a6c6d6cd1cc7438959a96a21c0
Anthima <anthima302@gmail.com>
2021-02-27 10:58:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cameraexample/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.cameraexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnCaptureImage;
+    ImageView imageDisplay;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCaptureImage =(Button) findViewById(R.id.btn_captureImage);
+        imageDisplay = (ImageView) findViewById(R.id.imageCapture);
+
+        btnCaptureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                startActivityForResult(intent, 0);
+
+            }
+        });
+}
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageDisplay.setImageBitmap(bitmap);
+
+    }
+
+
+}
